ed83f62efb5231b8be0cc7d8474c8bc42fee7f4f
abir-taheer <abir488@gmail.com>
2019-03-25 23:36:48 +0000

    fixed config comments

--- config.php
+++ config.php
@@ -9,14 +9,11 @@ const web_description = ""; // String, Metadata description for app
 const web_favicon = "";     // String, The absolute web path to the app favicon
 const web_domain = "";      // String, The domain on which the app is hosted, will be used for cookies
 const web_ssl = false;      // Boolean, Whether or not the site uses ssl and whether or not cookies should be made secure only
-
 /**
  * Google Analytics Configuration -- Use of Google analytics is optional, but google_analytics_use must be false if not being used
  */
 const google_analytics_use = true;      // Boolean, Whether or not to use Google Analytics on site
 const google_analytics_tag_id = "";     // String, The Tag ID of the Google Analytics property if Google Analytics is being used
-
-
 /**
  * Database configuration information -- Mandatory, Must be a MySQL Database
  */
@@ -24,25 +21,21 @@ const db_host = "";         // String, Database Host
 const db_name = "";         // String, Database Name
 const db_username = "";     // String, Database username
 const db_password = "";     // String, Database password
-
 /**
  * App Configuration Information -- Mandatory
  */
-const session_id_cookie = "";       // String, Name of cookie referencing ID information -- Can be left as is
-const session_voting_cookie = "";   // String, Name of cookie referencing voting information -- Can be left as is
+const session_id_cookie = "";       // String, Name of cookie referencing ID information -- Cannot be the same as session_voting_cookie
+const session_voting_cookie = "";   // String, Name of cookie referencing voting information -- Cannot be the same as session_id_cookie
 const google_auth_client_id = "";   // String, Client ID for Google Identity Platform,
                                         // Read more here: https://developers.google.com/identity/sign-in/web/sign-in
-const auth_allowed_org = "";            // String, Only allow users of a certain organization to be able to sign in
+const auth_allowed_org = "";        // String, Only allow users of a certain organization to be able to sign in
                                         // Or leave as "*" to allow all users
                                         // Accepted Format: "stuy.edu, bxsci.edu" or "*"
-
 const app_time_zone = "";           // String, Primary time zone for the users of the app
                                         // App will automatically adjust times on client's end to match server
                                         // Time Zone must be a supported PHP Time Zone:
                                         // https://www.php.net/manual/en/timezones.php
-
 const app_root = __dir__;           // DO NOT EDIT UNLESS APP ROOT IS DIFFERENT FROM LOCATION OF CONFIG FILE
-
 /**
  * Email configuration -- Mandatory!
  */
@@ -54,5 +47,3 @@ const smtp_secure_method = "";  // String, The method of which to communicate wi
 const smtp_port = 465;          // Int, The port to be used for the SMTP endpoint
 const smtp_from_email = "";     // String, The email with which outgoing emails will be sent from
 const smtp_from_name = "";      // String, The name that will be used for outgoing emails
-
-
