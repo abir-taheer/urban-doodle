aebd3eae8c67a5a4f5318fbc76a210ff3a2fca69
abir-taheer <abir488@gmail.com>
2019-05-06 07:10:08 +0000

    better phone number regex, cleaner ui

--- classes/BasicInfo.php
+++ classes/BasicInfo.php
@@ -20,17 +20,37 @@ class BasicInfo {
         switch(strtolower($this->type)){
             case "website":
             case "url":
-                $content = "<a href=\"http://".htmlspecialchars($this->content)."\" target=\"_blank\"'>".$this->content."</a>";
+                $content = "<a href=\"http://".htmlspecialchars($this->content)."\" target=\"_blank\"'>".htmlspecialchars($this->content)."</a>";
                 break;
             case "email":
             case "email address":
-                $content = "<a href=\"mailto:".htmlspecialchars($this->content)."\">".$this->content."</a>";
+                $content = "<a href=\"mailto:".htmlspecialchars($this->content)."\">".htmlspecialchars($this->content)."</a>";
                 break;
             case "phone":
             case "phone number":
             case "phone #":
-                $number = preg_replace('/[^+0-9]/', '', $this->content);
-                $content = "<a href=\"tel:".$number."\">".$this->content."</a>";
+                $number = $this->content;
+                preg_match_all("/[a-zA-Z]/", $this->content, $letter_matches);
+                $letter_associations = [
+                    "a-cA-C" => "2",
+                    "d-fD-F" => "3",
+                    "g-iG-I" => "4",
+                    "j-lJ-L" => "5",
+                    "m-oM-O" => "6",
+                    "p-sP-S" => "7",
+                    "t-vT-V" => "8",
+                    "w-zW-Z" => "9"
+                ];
+                foreach( $letter_matches[0] as $letter_match ){
+                    foreach( $letter_associations as $letter_range => $association ){
+                        if(preg_match("/[".$letter_range."]/", $letter_match)) {
+                            $number = str_replace($letter_match, $association, $number);
+                            break;
+                        }
+                    }
+                }
+                $number = preg_replace('/[^+0-9]/', '', $number);
+                $content = "<a href=\"tel:".$number."\">".htmlspecialchars($this->content)."</a>";
                 break;
             default:
                 $content = htmlspecialchars($this->content);
